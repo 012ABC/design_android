da538c967360ad083313e269afbc9c9787337de1
dylan121 <Zhanghyang@mysteel.com>
2018-07-17 11:01:27 +0800

    1.单一原则

--- app/src/main/java/com/example/abc/project_design1/MainActivity.java
+++ app/src/main/java/com/example/abc/project_design1/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.abc.project_design1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.example.abc.project_design1.utils.ImageLoader;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageLoader mImageLoder=new ImageLoader();
+    private ImageView mImage;
+    public static final String url="http://image.jisuxz.com/desktop/2031/jisu_awngziwen_30.jpg";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mImage = findViewById(R.id.img);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                mImageLoder.displayImage(url,mImage);
+            }
+        }).start();
+
     }
 }
--- app/src/main/java/com/example/abc/project_design1/utils/ImageLoader.java
+++ app/src/main/java/com/example/abc/project_design1/utils/ImageLoader.java
@@ -7,36 +7,32 @@ import android.widget.ImageView;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class ImageLoader {
 
-    LruCache<String,Bitmap>mImageCache;
+    //图片缓存
+    ImageCache mImageCache=new ImageCache();
+
+    //线程池，线程数量为CPU的数量
 
     ExecutorService mExecutorService= Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
-    public ImageLoader(){
-        initImageCache();
-    }
-
-    private void initImageCache() {
-        final int maxMemory=(int)(Runtime.getRuntime().maxMemory()/1024);
-        final int cacheSize=maxMemory/4;
-        mImageCache=new LruCache<String,Bitmap>(cacheSize){
-            @Override
-            protected int sizeOf(String key, Bitmap value) {
-                return value.getRowBytes()*value.getHeight()/1024;
-            }
-        };
-    }
     public void displayImage(final String url, final ImageView imageView){
+        final Bitmap bitmap=mImageCache.get(url);
+        if (bitmap!=null){
+            imageView.setImageBitmap(bitmap);
+            return;
+        }
         imageView.setTag(url);
         mExecutorService.submit(new Runnable() {
             @Override
             public void run() {
                 Bitmap bitmap=downloadImage(url);
+
                 if (bitmap==null){
                     return;
                 }
@@ -48,13 +44,15 @@ public class ImageLoader {
         });
     }
 
-    private Bitmap downloadImage(String imageUrl) {
+    public Bitmap downloadImage(String imageUrl) {
         Bitmap bitmap=null;
         try {
             URL url=new URL(imageUrl);
-            HttpURLConnection connection= (HttpURLConnection) url.openConnection();
-            bitmap= BitmapFactory.decodeStream(connection.getInputStream());
-            connection.disconnect();
+            HttpURLConnection conn= ((HttpURLConnection) url.openConnection());
+            bitmap=BitmapFactory.decodeStream(conn.getInputStream());
+            conn.disconnect();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
